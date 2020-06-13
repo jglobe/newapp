ca82190320a0b5004c5a22a42d61df94f137f292
jglobe <shrkss0@gmail.com>
2020-06-13 18:32:26 +0300

    less2

---
+++ src/components/SearchBlock.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchBlock = () => {
+    const searchText = 'enter what you search';
+    return <input placeholder={searchText}/>;
+  }
+
+export default SearchBlock;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBlock from './components/SearchBlock';
+const Header = () => {
+  const st = {
+    fontSize: '40px',
+    color:'aqua',
+  }
+  return <h1 style={st} className="header">To do list</h1>;
+}
 
-const el = <h1>React Element</h1>;
 
-ReactDOM.render(el, document.getElementById('root'));
-console.dir(document.querySelector('h1'));
+const ToDoList = () => {
+  const items = ['Learn HTML', 'Learn CSS','Learn React'];
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+    </ul>
+  );
+}
+
+
+
+const App = () => {
+
+const isloged = false;
+const loginBlock = <span>sign in</span>;
+const welcome = <span>Hello</span>
+
+  return (
+    <div>
+      {isloged ? welcome : loginBlock}
+      <Header />
+      <SearchBlock />
+      <ToDoList />
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
